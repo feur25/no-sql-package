5ac433088c22e892c6b6eb55c71b6f18d267e6f2
FeurKing <39668417+feur25@users.noreply.github.com>
2024-04-17 21:48:36 +0200

    Update importmongo.sh

--- importmongo.sh
+++ importmongo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 current_position=$(pwd)
-docker exec -i mongo_instance_1 mongoimport --db nosql --collection usersCollection --drop --jsonArray < "$current_position/json/users.json" 
+docker exec -i mongo_instance_1 mongoimport --db nosql --collection usersCollection --drop --jsonArray < "$current_position/CRUD/json/users.json" 
